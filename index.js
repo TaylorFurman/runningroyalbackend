87b85afb18719d4a5366ca3659a31bac92a0abd7
unknown <TFurman47@gmail.com>
2021-09-23 18:14:26 -0500

    update

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ app.use(express.urlencoded({extended:false}))
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-      res.sendFile(__dirname + '/index.html');
-})
+// app.get('/', (req, res) => {
+//       res.sendFile(__dirname + '/index.html');
+// })
 
 
 app.get('./src/api/run_history.json',(req,res)=>{
